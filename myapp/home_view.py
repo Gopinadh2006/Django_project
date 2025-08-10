a71514a732f16bbd12c31d1abdaedb805188fc8c
V.Gopinadh <166527417+Gopinadh2006@users.noreply.github.com>
2025-08-10 11:38:03 +0530

    home_view.py

--- myapp/home_view.py
+++ myapp/home_view.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 @login_required
 def home_view(request):
     return render(request, 'myapp/home.html')
+
